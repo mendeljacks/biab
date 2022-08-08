a28263798ab85c16f09bba1adb5942453267d092
Mendel <mendeljacks@gmail.com>
2022-08-08 11:18:17 -0400

    Added query and mutate route

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -1,19 +1,31 @@
 import cors from 'cors'
 import express from 'express'
 import { handler } from 'express_phandler'
+import { mutate_handler, query_handler } from '../config/orma'
+import { pool } from '../config/pg'
+import { introspect } from '../scripts/introspect'
 
 const port = process.env.PORT || 3001
 
 export const start = async (env: string) => {
     const app = express()
+    await introspect(env)
 
     app.use(cors())
     app.use(express.json({ limit: '50mb' }))
     app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-    app.get(
-        '/',
-        handler(() => 'Welcome')
+    app.post(
+        '/query',
+        handler(async (req: Request) => {
+            const results = await query_handler(req.body)
+            return results
+        })
+    )
+
+    app.post(
+        '/mutate',
+        handler(async req => mutate_handler(req.body))
     )
 
     await new Promise(r => app.listen(port, r as any))
---
+++ common/orma_schema.ts
@@ -0,0 +1,265 @@
+export const orma_schema = {
+  "migrations": {
+    "id": {
+      "data_type": "bigint",
+      "ordinal_position": 1,
+      "not_null": true,
+      "character_count": 64,
+      "default": "unique_rowid()"
+    },
+    "name": {
+      "data_type": "character varying",
+      "ordinal_position": 2,
+      "not_null": true,
+      "character_count": 255
+    },
+    "run_on": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 3,
+      "not_null": true,
+      "decimal_places": 6
+    },
+    "$indexes": [
+      {
+        "index_name": "migrations_pkey",
+        "is_unique": true,
+        "fields": [
+          "id"
+        ],
+        "invisible": false
+      }
+    ]
+  },
+  "users": {
+    "id": {
+      "data_type": "bigint",
+      "ordinal_position": 1,
+      "not_null": true,
+      "primary_key": true,
+      "character_count": 64,
+      "default": "nextval('public.users_id_seq'::REGCLASS)"
+    },
+    "email": {
+      "data_type": "character varying",
+      "ordinal_position": 2,
+      "not_null": true,
+      "character_count": 10485760
+    },
+    "password": {
+      "data_type": "character varying",
+      "ordinal_position": 3,
+      "not_null": true,
+      "character_count": 10485760
+    },
+    "first_name": {
+      "data_type": "character varying",
+      "ordinal_position": 4,
+      "character_count": 10485760
+    },
+    "last_name": {
+      "data_type": "character varying",
+      "ordinal_position": 5,
+      "character_count": 10485760
+    },
+    "phone": {
+      "data_type": "character varying",
+      "ordinal_position": 6,
+      "character_count": 10485760
+    },
+    "created_at": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 7,
+      "not_null": true,
+      "decimal_places": 6,
+      "default": "now():::TIMESTAMP"
+    },
+    "updated_at": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 8,
+      "not_null": true,
+      "decimal_places": 6,
+      "default": "now():::TIMESTAMP"
+    },
+    "resource_id": {
+      "data_type": "character varying",
+      "ordinal_position": 9,
+      "not_null": true,
+      "character_count": 10485760
+    },
+    "$indexes": [
+      {
+        "index_name": "users_email_uq",
+        "is_unique": true,
+        "fields": [
+          "email"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "users_phone_uq",
+        "is_unique": true,
+        "fields": [
+          "phone"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "users_resource_id_uq",
+        "is_unique": true,
+        "fields": [
+          "resource_id"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "users_pkey",
+        "is_unique": true,
+        "fields": [
+          "id"
+        ],
+        "invisible": false
+      }
+    ]
+  },
+  "roles": {
+    "id": {
+      "data_type": "bigint",
+      "ordinal_position": 1,
+      "not_null": true,
+      "primary_key": true,
+      "character_count": 64,
+      "default": "nextval('public.roles_id_seq'::REGCLASS)"
+    },
+    "name": {
+      "data_type": "character varying",
+      "ordinal_position": 2,
+      "not_null": true,
+      "character_count": 10485760
+    },
+    "created_at": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 3,
+      "not_null": true,
+      "decimal_places": 6,
+      "default": "now():::TIMESTAMP"
+    },
+    "updated_at": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 4,
+      "not_null": true,
+      "decimal_places": 6,
+      "default": "now():::TIMESTAMP"
+    },
+    "resource_id": {
+      "data_type": "character varying",
+      "ordinal_position": 5,
+      "not_null": true,
+      "character_count": 10485760
+    },
+    "$indexes": [
+      {
+        "index_name": "roles_name_uq",
+        "is_unique": true,
+        "fields": [
+          "name"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "roles_resource_id_uq",
+        "is_unique": true,
+        "fields": [
+          "resource_id"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "roles_pkey",
+        "is_unique": true,
+        "fields": [
+          "id"
+        ],
+        "invisible": false
+      }
+    ]
+  },
+  "user_has_roles": {
+    "id": {
+      "data_type": "bigint",
+      "ordinal_position": 1,
+      "not_null": true,
+      "primary_key": true,
+      "character_count": 64,
+      "default": "nextval('public.user_has_roles_id_seq'::REGCLASS)"
+    },
+    "user_id": {
+      "data_type": "bigint",
+      "ordinal_position": 2,
+      "not_null": true,
+      "character_count": 64,
+      "references": {
+        "users": {
+          "id": {}
+        }
+      }
+    },
+    "role_id": {
+      "data_type": "bigint",
+      "ordinal_position": 3,
+      "not_null": true,
+      "character_count": 64,
+      "references": {
+        "roles": {
+          "id": {}
+        }
+      }
+    },
+    "created_at": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 4,
+      "not_null": true,
+      "decimal_places": 6,
+      "default": "now():::TIMESTAMP"
+    },
+    "updated_at": {
+      "data_type": "timestamp without time zone",
+      "ordinal_position": 5,
+      "not_null": true,
+      "decimal_places": 6,
+      "default": "now():::TIMESTAMP"
+    },
+    "resource_id": {
+      "data_type": "character varying",
+      "ordinal_position": 6,
+      "not_null": true,
+      "character_count": 10485760
+    },
+    "$indexes": [
+      {
+        "index_name": "user_has_roles_user_id_role_id_uq",
+        "is_unique": true,
+        "fields": [
+          "role_id",
+          "user_id"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "user_has_roles_resource_id_uq",
+        "is_unique": true,
+        "fields": [
+          "resource_id"
+        ],
+        "invisible": false
+      },
+      {
+        "index_name": "user_has_roles_pkey",
+        "is_unique": true,
+        "fields": [
+          "id"
+        ],
+        "invisible": false
+      }
+    ]
+  }
+} as const
