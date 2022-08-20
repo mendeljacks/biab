b8ccecd4113f93aa1208270b185caf42852bd62e
Mendel <mendeljacks@gmail.com>
2022-08-20 18:44:05 -0400

    add to prepopulate

--- backend/src/scripts/prepopulate.ts
+++ backend/src/scripts/prepopulate.ts
@@ -5,7 +5,15 @@ export const populated_data = {
     roles: [
         { id: 1, name: 'admin' },
         { id: 2, name: 'user' }
-    ]
+    ],
+    users: [
+        {
+            id: 1,
+            email: 'admin',
+            password: '$2b$10$Cj.60A.stZiMz7wUpXAtAOeZHsqAwme3G1Qoxv0T.74tXOV3nlzca'
+        }
+    ],
+    user_has_roles: [{ id: 1, user_id: 1, role_id: 1 }]
 }
 
 export const prepopulate = async () => {
