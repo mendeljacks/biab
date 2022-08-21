1d574e6bedcd762029bbd5dfbb665d586c85d9a0
Mendel <mendeljacks@gmail.com>
2022-08-21 19:57:57 -0400

    docker

---
+++ backend/src/hosting/deploy.ts
@@ -0,0 +1,42 @@
+import cuid from 'cuid'
+import git from 'git-rev-sync'
+import { run_process } from './helpers/run_process'
+
+export const deploy = async () => {
+    try {
+        process.chdir('../')
+        const version = 'GIT_' + git.short() + '_GID_' + cuid()
+
+        await Promise.all([aws(version)])
+
+        await run_process(['docker', 'image', 'prune', '-af'])
+        console.log('Done')
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+const aws = async version => {
+    try {
+        // Run aws configure
+        // Login with: aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 684954451958.dkr.ecr.us-east-1.amazonaws.com
+
+        const image = `684954451958.dkr.ecr.us-east-1.amazonaws.com/clubs:${version}`
+
+        await run_process([
+            'docker',
+            'build',
+            '-f',
+            'backend/src/hosting/Dockerfile',
+            '--tag',
+            image,
+            '.'
+        ])
+        await run_process(['docker', 'push', image])
+    } catch (error) {
+        console.error('Failed to deploy')
+        console.error(error)
+        process.exit(1)
+    }
+}
