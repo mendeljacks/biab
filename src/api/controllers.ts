c3bc09f28bad42b07e5ed9540db6af650cbf3147
Mendel <mendeljacks@gmail.com>
2022-08-26 15:51:23 -0400

    improve welcome message

--- src/api/controllers.ts
+++ src/api/controllers.ts
@@ -3,17 +3,17 @@ import { authenticate } from './auth/auth'
 import { ensure_ownership } from './auth/ownership'
 import { ensure_perms } from './auth/perms'
 
-export const welcome = async _ => `Welcome to clubs app. ${process.env.SERVER_ROOT_URI}`
+export const welcome = async (to: string) => `Welcome to ${to}`
 
-export const query = async req => {
-    const token_content = await authenticate(req, process.env.jwt_secret)
+export const query = async (req, jwt_secret: string) => {
+    const token_content = await authenticate(req, jwt_secret)
     await ensure_perms(req.body, token_content, 'query')
     await ensure_ownership(req.body, token_content, 'query')
     return query_handler(req.body)
 }
 
-export const mutate = async req => {
-    const token_content = await authenticate(req, process.env.jwt_secret)
+export const mutate = async (req, jwt_secret: string) => {
+    const token_content = await authenticate(req, jwt_secret)
     await ensure_perms(req.body, token_content, 'mutate')
     await ensure_ownership(req.body, token_content, 'mutate')
     return mutate_handler(req.body)
