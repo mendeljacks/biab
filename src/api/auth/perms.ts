2c20576caf5df0c3686b2a29da66c9d46eb7d0e3
Mendel <mendeljacks@gmail.com>
2022-11-13 11:54:03 -0500

    Don't require perms for $supersede keyword

--- src/api/auth/perms.ts
+++ src/api/auth/perms.ts
@@ -33,6 +33,9 @@ export const ensure_perms = async (
     let needed_perms = {}
     const deep_for_each = mode === 'query' ? query_for_each : mutation_entity_deep_for_each
     deep_for_each(query, (value, path, entity_name) => {
+        if (entity_name.startsWith('$')) {
+            return
+        }
         if (!needed_perms[entity_name]) needed_perms[entity_name] = {}
         if (!Object.keys(role_has_perms).includes(entity_name)) {
             throw new Error(`No permissions defined for entity ${entity_name}`)
